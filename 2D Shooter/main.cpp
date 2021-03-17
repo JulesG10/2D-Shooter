38a2c09fc8aa0023364f02d4126cebac7fac35e4
Jules <60539590+JulesG10@users.noreply.github.com>
2021-03-17 16:55:35 +0100

    Fix

--- 2D Shooter/main.cpp
+++ 2D Shooter/main.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 #include "Game.h"
-#include "Macro.hpp"
+#include<Windows.h>
 
 int main(int argc,char* argv[])
 {
+	#ifndef _DEBUG
+		ShowWindow(GetConsoleWindow(), SW_HIDE);
+	#endif // !_DEBUG
+
 	std::string start = argv[0];
 	while (start[start.size() - 1] != '\\')
 	{
 		start.resize(start.size() - 1);
 	}
 
-	Game* g =new Game();
+	Game* g = new Game();
 	g->Run(start);
 	
 	return 0;
